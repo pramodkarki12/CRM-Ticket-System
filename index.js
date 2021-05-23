bc5319aa276e577ec7c8c626e84861acc9457c00
pramodkarki12 <np03a190229@heraldcollege.edu.np>
2021-05-23 13:56:52 +0545

    new file added

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Hello Coder");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+// Configuration
+const app = express();
+const router = express.Router();
+
+// PORT
+const port = process.env.PORT || 5000;
+
+// API Security
+app.use(helmet());
+
+// Handle CORS error
+app.use(cors());
+
+// Logger
+app.use(morgan()); 
+
+app.use('/', (req, res, next) => {
+  res.json({ message: 'Hello Coder' });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on PORT: ${port}`);
+});
